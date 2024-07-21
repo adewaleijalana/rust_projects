9a23b1f88485f6178d1c74986dfc03bed9b7b7b7
adewale-pathfound <adewale@pathfound.io>
2024-07-21 19:44:21 +0100

    code organization and lifetime annotation

--- life_time_example/src/main.rs
+++ life_time_example/src/main.rs
@@ -1,44 +1,15 @@
-use std::fmt::Display;
+mod lifetime;
+
+#[allow(unused_imports)]
+use lifetime::{longest, test_life_time};
 
 fn main() {
-    //1
-//    let string1 = String::from("abcd");
-//     let string2 = "xyz";
+    //    test_life_time::lifetime_test_error();
+    //    test_life_time::lifetime_test_2()
 
-//     let result = longest(string1.as_str(), string2);
-//     println!("The longest string is {result}");
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
 
-//2
-    // let string1 = String::from("long string is long");
-    // {
-    //     let string2 = String::from("xyz");
-    //     let result = longest(string1.as_str(), string2.as_str());
-    //     println!("The longest string is: {result}");
-    // }
-//3ASWEFT
-    // let string1 = String::from("long string is long");
-    // let result;
-
-    // {
-    //     let string2 = String::from("xyz");
-    //     result = longest(string1.as_str(), string2.as_str());
-    // }
-    // println!("The longest string is: {result}");
-}
-
-// fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-//     if x.len() > y.len() {
-//         x
-//     } else {
-//         y
-//     }
-// }
-
-fn longest<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str where T : Display{
-    print!("Announcement! {ann}");
-    if x.len() > y.len() {
-        x
-    } else {
-        y
-    }
+    let result = longest::longest(string1.as_str(), string2);
+    println!("The longest string is {result}");
 }
--- life_time_example/target/debug/deps/life_time_example-deeba5649dbd5437.d
+++ life_time_example/target/debug/deps/life_time_example-deeba5649dbd5437.d
@@ -1,5 +1,8 @@
-/Users/adewaleijalana/Documents/rust_projects/life_time_example/target/debug/deps/life_time_example-deeba5649dbd5437: src/main.rs
+/Users/adewaleijalana/Documents/rust_projects/life_time_example/target/debug/deps/life_time_example-deeba5649dbd5437: src/main.rs src/lifetime.rs src/lifetime/test_life_time.rs src/lifetime/longest.rs
 
-/Users/adewaleijalana/Documents/rust_projects/life_time_example/target/debug/deps/life_time_example-deeba5649dbd5437.d: src/main.rs
+/Users/adewaleijalana/Documents/rust_projects/life_time_example/target/debug/deps/life_time_example-deeba5649dbd5437.d: src/main.rs src/lifetime.rs src/lifetime/test_life_time.rs src/lifetime/longest.rs
 
 src/main.rs:
+src/lifetime.rs:
+src/lifetime/test_life_time.rs:
+src/lifetime/longest.rs:
