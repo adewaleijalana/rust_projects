c274a7c92eeadabb88a54dc269209dec0fa3624e
adewale-pathfound <adewale@pathfound.io>
2024-07-21 21:00:55 +0100

    code organization and lifetime annotation

--- life_time_example/target/debug/deps/life_time_example-7ccec4ab24499f0b.d
+++
@@ -1,8 +0,0 @@
-/Users/adewaleijalana/Documents/rust_projects/life_time_example/target/debug/deps/liblife_time_example-7ccec4ab24499f0b.rmeta: src/main.rs src/lifetime.rs src/lifetime/test_life_time.rs src/lifetime/longest.rs
-
-/Users/adewaleijalana/Documents/rust_projects/life_time_example/target/debug/deps/life_time_example-7ccec4ab24499f0b.d: src/main.rs src/lifetime.rs src/lifetime/test_life_time.rs src/lifetime/longest.rs
-
-src/main.rs:
-src/lifetime.rs:
-src/lifetime/test_life_time.rs:
-src/lifetime/longest.rs:
--- life_time_example/target/debug/deps/life_time_example-fa27fac516f426e1.d
+++
@@ -1,8 +0,0 @@
-/Users/adewaleijalana/Documents/rust_projects/life_time_example/target/debug/deps/liblife_time_example-fa27fac516f426e1.rmeta: src/main.rs src/lifetime.rs src/lifetime/test_life_time.rs src/lifetime/longest.rs
-
-/Users/adewaleijalana/Documents/rust_projects/life_time_example/target/debug/deps/life_time_example-fa27fac516f426e1.d: src/main.rs src/lifetime.rs src/lifetime/test_life_time.rs src/lifetime/longest.rs
-
-src/main.rs:
-src/lifetime.rs:
-src/lifetime/test_life_time.rs:
-src/lifetime/longest.rs:
