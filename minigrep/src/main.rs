0451b92604c0c37d7749f933af6367eeaa61e141
adewale-pathfound <adewale@pathfound.io>
2024-08-07 22:36:47 +0100

    add associated function to Config struct

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,25 +3,25 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
     let contents = fs::read_to_string(config.file_path).expect("Error reading the file");
     println!("With text:\n{contents}");
-
-}
-
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Config{query, file_path}
 }
 
 struct Config {
     query: String,
-    file_path: String
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
+    }
 }
