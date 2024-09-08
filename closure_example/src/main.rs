f90f1ee4263c75abf43a04e6287cc909ad7a4bac
adewaleijalana <adewaleijalana@gmail.com>
2024-09-08 09:46:52 +0100

    use iterator instead of loops in the search methods

--- closure_example/src/main.rs
+++ closure_example/src/main.rs
@@ -1,3 +1,30 @@
+use std::thread;
+
+
 fn main() {
-    println!("Hello, world!");
+    // borrows_mutably_ex();
+    closure_move_ex();
+}
+
+#[allow(dead_code)]
+fn borrows_mutably_ex() {
+    let mut list = vec![1, 2, 3];
+    println!("Before defining closure: {list:?}");
+
+    let mut borrows_mutably = || {
+        list.push(7);
+    };
+
+    borrows_mutably();
+    println!("After calling closure: {list:?}");
+}
+
+#[allow(dead_code)]
+fn closure_move_ex() {
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {list:?}");
+
+    thread::spawn(move || println!("From thread: {list:?}"))
+        .join()
+        .unwrap();
 }
--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -9,7 +9,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // println!("With text:\n{contents}");
     let result = if config.ignore_case {
         search_case_insensitive(&config.query, &contents)
-    }else{
+    } else {
         search(&config.query, &contents)
     };
 
@@ -20,24 +20,34 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line)
-        }
-    }
-    results
+    // let mut results = Vec::new();
+    // for line in contents.lines() {
+    //     if line.contains(query) {
+    //         results.push(line)
+    //     }
+    // }
+    // results
+
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
-pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-    results
+    // let mut results = Vec::new();
+    // for line in contents.lines() {
+    //     if line.to_lowercase().contains(&query) {
+    //         results.push(line);
+    //     }
+    // }
+    // results
+
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
@@ -46,7 +56,7 @@ mod tests {
     use super::*;
 
     #[test]
-     fn case_sensitive() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
