391febb7b17bfd86b424485136417e4f7eab407f
adewaleijalana <adewaleijalana@gmail.com>
2024-09-13 21:37:17 +0100

    running tests in workspace

--- bills_pay/bills_pay_domain/src/lib.rs
+++ bills_pay/bills_pay_domain/src/lib.rs
@@ -2,13 +2,12 @@ pub fn domain_test() {
     println!("Inside domain library")
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
+    #[test]
+    fn it_works() {
+        domain_test();
+    }
+}
