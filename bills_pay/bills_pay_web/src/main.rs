195cbc67c1dbface7b3257552aef5c0be8178a59
adewaleijalana <adewaleijalana@gmail.com>
2024-09-14 00:09:36 +0100

    running tests in workspace

--- bills_pay/bills_pay_web/src/main.rs
+++ bills_pay/bills_pay_web/src/main.rs
@@ -1,5 +1,30 @@
+use std::ops::Deref;
+
 use bills_pay_service;
 fn main() {
     println!("Hello, Workspace from the main method!");
     bills_pay_service::service_test();
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+
+struct MyBox<T>(T);
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    
+    fn deref(&self) -> &Self::Target {
+        todo!()
+    }
+}
+
+impl<T>  MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
 }
