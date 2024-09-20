21abc1bb11d886b2a1273ec91c015a84c372b896
adewaleijalana <adewaleijalana@gmail.com>
2024-09-20 23:06:08 +0100

    smart pointer

--- bills_pay/bills_pay_web/src/main.rs
+++ bills_pay/bills_pay_web/src/main.rs
@@ -2,14 +2,19 @@ use std::ops::Deref;
 
 use bills_pay_service;
 fn main() {
-    println!("Hello, Workspace from the main method!");
-    bills_pay_service::service_test();
+    // println!("Hello, Workspace from the main method!");
+    // bills_pay_service::service_test();
 
-    let x = 5;
-    let y = MyBox::new(x);
+    // let x = 5;
+    // let y = MyBox::new(x);
 
-    assert_eq!(5, x);
-    assert_eq!(5, *y);
+    // assert_eq!(5, x);
+    // assert_eq!(5, *y);
+
+    let nums = [22, 3, 45, 5, 65];
+
+    let result = largest_number(&nums);
+    println!("largest number is {result}")
 }
 
 
@@ -19,7 +24,7 @@ impl<T> Deref for MyBox<T> {
     type Target = T;
     
     fn deref(&self) -> &Self::Target {
-        todo!()
+        &self.0
     }
 }
 
@@ -27,4 +32,18 @@ impl<T>  MyBox<T> {
     fn new(x: T) -> MyBox<T> {
         MyBox(x)
     }
+}
+
+fn largest_number(nums: &[i32]) -> i32 {
+    let mut max = nums[0];
+
+    // let res = nums.iter().max().unwrap();
+    // println!("{res}");
+
+    for num in nums.iter() {
+        if *num > max {
+            max = *num;
+        }
+    }
+    max
 }
