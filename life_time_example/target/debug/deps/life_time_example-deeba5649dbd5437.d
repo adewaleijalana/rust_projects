b2f3cffe76bd055c748a2191dfc680ecc639dc3f
adewaleijalana <adewaleijalana@gmail.com>
2024-10-05 20:50:48 +0100

    removed target folder

--- life_time_example/target/debug/deps/life_time_example-deeba5649dbd5437.d
+++
@@ -1,8 +0,0 @@
-/Users/adewaleijalana/Documents/rust_projects/life_time_example/target/debug/deps/life_time_example-deeba5649dbd5437: src/main.rs src/lifetime.rs src/lifetime/test_life_time.rs src/lifetime/longest.rs
-
-/Users/adewaleijalana/Documents/rust_projects/life_time_example/target/debug/deps/life_time_example-deeba5649dbd5437.d: src/main.rs src/lifetime.rs src/lifetime/test_life_time.rs src/lifetime/longest.rs
-
-src/main.rs:
-src/lifetime.rs:
-src/lifetime/test_life_time.rs:
-src/lifetime/longest.rs:
