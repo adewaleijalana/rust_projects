9aef9febe7e4f870617d34a9cb49d73470f18ee7
adewaleijalana <adewaleijalana@gmail.com>
2025-11-23 21:16:23 +0100

    closure examples with FnMut

--- closure_example/src/functions.rs
+++ closure_example/src/functions.rs
@@ -1,4 +1,4 @@
-use std::thread;
+use std::{io::ErrorKind, thread};
 
 pub fn borrows_mutably_ex() {
     let mut list = vec![1, 2, 3];
@@ -27,12 +27,50 @@ pub fn borrows_immutably_ex() {
     let list = vec![1, 2, 3];
     println!("Before defining closure: {list:?}");
 
-    let borrows_immutably = || {
-        println!("Inside a closure{:?}", list)
-    };
+    let borrows_immutably = || println!("Inside a closure{:?}", list);
 
     borrows_immutably();
     borrows_immutably();
     borrows_immutably();
     println!("After calling closure: {list:?}");
+}
+
+pub fn unwrap_or_else_test_option() {
+    let option = Some("Salamin");
+    let food = option.unwrap_or_else(|| "Pizza");
+    println!("Food from Some(x): {food}");
+
+    let option = None;
+    let food = option.unwrap_or_else(|| "Pizza");
+    println!("Food from None: {food}");
+}
+
+pub fn unwrap_or_else_test_result() {
+    let result = Ok("Salamin");
+    let food = result.unwrap_or_else(|_: &str| "Pizza");
+    println!("Food from Ok(x): {food}");
+
+    let result = Err("test");
+    let food = result.unwrap_or_else(|e| {
+        if e.eq_ignore_ascii_case("Test") {
+            "Pizza"
+        } else {
+            "Bugger"
+        }
+    });
+
+    // let result = Err(ErrorKind::InvalidData);
+    // let result = Err(ErrorKind::InvalidInput);
+    // let food = result.unwrap_or_else(|e| match e {
+    //     ErrorKind::InvalidData => "Pizza",
+    //     _ => "Bugger",
+    // });
+
+    println!("Food from Err(e): {food}");
+}
+
+pub fn closure_function<F>(f : F)
+where F: FnOnce()
+{
+    f();
 }
--- closure_example/src/main.rs
+++ closure_example/src/main.rs
@@ -1,11 +1,40 @@
-#![allow(dead_code)]
+#![allow(dead_code, unused_imports)]
 
 mod functions;
+mod location;
 use functions::*;
+use location::{Location, Map};
 
 
 fn main() {
-    borrows_mutably_ex();
+    // borrows_mutably_ex();
     // borrows_immutably_ex();
     // closure_move_ex();
+
+    // unwrap_or_else_test_option();
+    // unwrap_or_else_test_result();
+
+    // let closure_method = || println!("Closure Method");
+
+    // closure_function(closure_method)
+
+    let locations = vec![Location::new(String::from("Test1"), 5), Location::new(String::from("Test2"), 13)];
+
+    let map = Map::new(&locations);
+
+    let mut total_treasures = 0;
+
+    let mut location_name = Vec::<String>::new();
+
+    map.explore(|location| {
+        total_treasures += location.treasure();
+    });
+
+    println!("Total number of treasures: {}", total_treasures);
+
+    map.explore(|loc| {
+        location_name.push(loc.name());
+    });
+
+    println!("Location name: {:?}", location_name);
 }
