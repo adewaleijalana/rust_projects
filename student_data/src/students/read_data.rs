ebb98a32f8f5f26065a27cb2b7f362291dd107b1
adewaleijalana <adewaleijalana@gmail.com>
2025-12-06 21:31:18 +0100

    option to result

--- student_data/src/students/read_data.rs
+++ student_data/src/students/read_data.rs
@@ -1,22 +1,24 @@
-use std::io::{self, BufRead, BufReader};
 use std::fs::{self, File};
+use std::io::{self, BufRead, BufReader, Error};
 
 use crate::students::student_data::Student;
 
-pub fn read_student_file() -> io::Result<Vec<Student>>{
+pub fn read_student_file() -> io::Result<Vec<Student>> {
+    let mut students = Vec::<Student>::new();
 
-  let mut students = Vec::<Student>::new();
+    let file = File::open("student_records.txt")?;
 
-  let file = File::open("student_records.txt")?;
+    let reader = BufReader::new(file);
 
-  let reader = BufReader::new(file);
-
-  for line_result in reader.lines().skip(1) {
-        let line = line_result?;
-        if let Some(student) = Student::parse_student(line){
-          students.push(student);
-        }
+    for line_result in reader.lines().skip(1) {
+        let line = line_result?; // Handle potential errors reading a line
+        // if let Some(student) = Student::parse_student(line) {
+        //     students.push(student);
+        // }
+        let std = Student::parse_student(line)
+            .ok_or_else(|| Error::new(std::io::ErrorKind::InvalidData, "bad line"))?;
+        // println!("{}", line);
+        students.push(std);
     }
-  Ok(students)
-    
+    Ok(students)
 }
