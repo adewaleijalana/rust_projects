272e6410d47a7881a9b2ebf3a0efbed91624c945
adewaleijalana <adewaleijalana@gmail.com>
2025-12-10 23:15:38 +0100

    pretty assertion

--- rose_museum/src/museum.rs
+++ rose_museum/src/museum.rs
@@ -27,11 +27,30 @@ impl Museum {
 #[cfg(test)]
 mod museum_tests {
     use super::*;
+    use pretty_assertions::{assert_eq, assert_ne};
 
     #[test]
     fn museum_sells_ticket_to_increase_revenue() {
         let mut museum = Museum::new();
         museum.sell_ticket();
         assert_eq!(museum.revenue, 25);
+    }
+
+    #[test]
+    fn museum_can_sell_multiple_tickets() {
+        let mut museum = Museum::new();
+        museum.sell_ticket();
+        museum.sell_ticket();
+        assert_eq!(museum.revenue, 55, "The revenue failed");
+    }
+
+    #[test]
+    fn museum_has_impressive_colletion() {
+      let mut museum = Museum::new();
+       museum.buy_painting("Monalisa");
+       museum.buy_painting("Monalisa");
+       museum.buy_painting("Monalisa");
+
+       assert!(museum.has_impressive_colletion());
     }
 }
