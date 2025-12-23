6da32a8c04dc04dd0b450d8b468e2b07940ff412
adewaleijalana <adewaleijalana@gmail.com>
2025-12-23 12:58:29 +0100

    add date and timedelta

--- datetime_with_rust/src/main.rs
+++ datetime_with_rust/src/main.rs
@@ -1,3 +1,16 @@
+#![allow(unused)]
+
+use chrono::{NaiveDate, TimeDelta};
+use std::ops::{Add,Sub};
 fn main() {
-    println!("Hello, world!");
+
+    let date = NaiveDate::from_ymd_opt(2025, 12, 23);
+
+    let date = date.unwrap();
+
+    println!("{date:?}");
+
+    let date_plus_5_days = date.add(TimeDelta::days(5));
+
+    println!("{date_plus_5_days:?}");
 }
