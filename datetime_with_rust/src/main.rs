8c9ba0518a2971f3135654b6a17274a86563f596
adewaleijalana <adewaleijalana@gmail.com>
2025-12-24 02:35:44 +0100

    add date with timezone

--- datetime_with_rust/src/main.rs
+++ datetime_with_rust/src/main.rs
@@ -1,16 +1,33 @@
 #![allow(unused)]
 
-use chrono::{NaiveDate, TimeDelta};
+use chrono::prelude::*;
+use chrono_tz::Africa::Lagos;
 use std::ops::{Add,Sub};
 fn main() {
 
-    let date = NaiveDate::from_ymd_opt(2025, 12, 23);
+    // let date = NaiveDate::from_ymd_opt(2025, 12, 23);
 
-    let date = date.unwrap();
+    // let date = date.unwrap();
 
-    println!("{date:?}");
+    // println!("{date:?}");
 
-    let date_plus_5_days = date.add(TimeDelta::days(5));
+    // let date_plus_5_days = date.add(TimeDelta::days(5));
 
-    println!("{date_plus_5_days:?}");
+    // println!("{date_plus_5_days:?}");
+    let system_time = Local::now();
+    let utc_time = Utc::now();
+
+    println!("{}", system_time.date_naive());
+
+    println!("{}", utc_time.date_naive());
+
+    println!("{}", system_time.date_naive());
+
+    println!("{}", system_time.time());
+
+    println!("{}", utc_time.time());
+
+    println!("{}", system_time.offset());
+
+    println!("{}", utc_time.offset());
 }
