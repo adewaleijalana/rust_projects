036195eb064094fcd6c7813d7d8d907e2319e7a3
adewaleijalana <adewaleijalana@gmail.com>
2025-12-27 19:13:18 +0100

    add method to insert and search methods

--- rust_pointers_examples/src/binary_search_tree.rs
+++ rust_pointers_examples/src/binary_search_tree.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 #[derive(Debug)]
 pub enum BinarySearchTree {
     Empty,
@@ -8,9 +10,45 @@ pub enum BinarySearchTree {
     },
 }
 
-
 impl BinarySearchTree {
-    pub fn new() -> Self{
+    pub fn new() -> Self {
         BinarySearchTree::Empty
+    }
+
+    pub fn insert(&mut self, new_value: i32) {
+        match self {
+            BinarySearchTree::Empty => {
+                let new_node = BinarySearchTree::Node {
+                    value: new_value,
+                    left: Box::new(BinarySearchTree::Empty),
+                    right: Box::new(BinarySearchTree::Empty),
+                };
+                *self = new_node;
+            }
+            BinarySearchTree::Node { value, left, right } => match new_value.cmp(value) {
+                Ordering::Equal => (),
+                Ordering::Greater => {
+                    right.insert(new_value);
+                }
+                Ordering::Less => {
+                    left.insert(new_value);
+                }
+            },
+        }
+    }
+
+    pub fn contains(&self, target: i32) -> bool {
+        match self {
+            BinarySearchTree::Empty => false,
+            BinarySearchTree::Node { value, left, right } => match target.cmp(value) {
+                Ordering::Equal => true,
+                Ordering::Greater => {
+                    right.contains(target)
+                }
+                Ordering::Less => {
+                    left.contains(target)
+                }
+            },
+        }
     }
 }
--- rust_pointers_examples/src/main.rs
+++ rust_pointers_examples/src/main.rs
@@ -7,11 +7,24 @@ mod music_playlist_item;
 use functions::*;
 use linked_list::LinkedList;
 
-use crate::music_playlist_item::MusicPlaylistItem;
+use crate::{binary_search_tree::BinarySearchTree, music_playlist_item::MusicPlaylistItem};
 fn main() {
     // raw_pointer_test();
     // smart_pointer();
 
+    let mut binary_search_tree = BinarySearchTree::new();
 
-    
+    binary_search_tree.insert(5);
+
+    binary_search_tree.insert(2);
+
+     binary_search_tree.insert(8);
+
+     binary_search_tree.insert(7);
+
+     binary_search_tree.insert(1);
+
+    println!("{:#?}", binary_search_tree);
+
+    println!("{}", binary_search_tree.contains(5))
 }
