2fe210ba179e179078a2297f2849ed241db43b6e
adewaleijalana <adewaleijalana@gmail.com>
2025-12-30 18:43:26 +0100

    add user routes

--- rocket_app/src/main.rs
+++ rocket_app/src/main.rs
@@ -1,5 +1,13 @@
+#![allow(unused)]
+
 #[macro_use]
 extern crate rocket;
+
+mod routes;
+
+use routes::users::users_routes::{
+    add_user, delete_users_by_id, get_users, get_users_by_id, update_user,
+};
 
 #[get("/")]
 fn index() -> &'static str {
@@ -8,5 +16,18 @@ fn index() -> &'static str {
 
 #[rocket::main]
 async fn main() {
-    let _ = rocket::build().mount("/", routes![index]).launch().await;
+    let _ = rocket::build()
+        .mount("/", routes![index])
+        .mount(
+            "/",
+            routes![
+                get_users,
+                get_users_by_id,
+                add_user,
+                update_user,
+                delete_users_by_id
+            ],
+        )
+        .launch()
+        .await;
 }
---
+++ rocket_app/src/routes/users/users_routes.rs
@@ -0,0 +1,26 @@
+use rocket::{response::status, serde::json::{Value, json}};
+
+#[get("/users")]
+pub fn get_users() -> Value {
+   json!([{"id": 1, "name": "Tester 1"}, {"id": 2, "name": "Tester 2"}])
+}
+
+#[get("/users/<id>")]
+pub fn get_users_by_id(id: u32) -> Value {
+    json!([{"id": 1, "name": "Tester 1"}])
+}
+
+#[post("/users", format = "json")]
+pub fn add_user() -> Value {
+   json!([{"id": 3, "name": "Tester 1"}])
+}
+
+#[put("/users/<id>", format = "json")]
+pub fn update_user(id: u32) -> Value {
+   json!([{"id": id, "name": "Tester 1"}])
+}
+
+#[delete("/users/<id>")]
+pub fn delete_users_by_id(id: u32) -> status::NoContent {
+    status::NoContent
+}
