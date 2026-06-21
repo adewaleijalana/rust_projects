4335106452a02f9bc24db2d7fc01241c4ef8e1b7
adewaleijalana <adewaleijalana@gmail.com>
2026-06-21 20:56:06 +0100

    using lib

--- rust-in-action/grep-lite/src/main.rs
+++ rust-in-action/grep-lite/src/main.rs
@@ -1,54 +1,7 @@
-use clap::{Arg, command};
-use regex::Regex;
-use std::fs::File;
-use std::io::{self, BufRead, BufReader};
+use grep_lite::reader_args;
 
 fn main() {
-    let args = command!()
-        .version("0.1")
-        .about("searches for patterns")
-        .arg(
-            Arg::new("pattern")
-                .help("The pattern to search for")
-                .required(true),
-        )
-        .arg(Arg::new("input").help("File to search").required(true))
-        .get_matches();
 
-    let pattern = args
-        .get_one::<String>("pattern")
-        .expect("`pattern`is required");
-
-    let binding = "-".to_string();
-    let input = args.get_one::<String>("input").unwrap_or(&binding);
-
-    let search_reg = Regex::new(pattern).unwrap();
-    //     let quote = "Every face, every shop, bedroom window, public-house, and
-    // dark square is a picture feverishly turned--in search of what?
-    // It is the same with books. What do we seek through millions of pages?";
-
-    if *input == binding {
-        let stdin = io::stdin();
-        let reader = stdin.lock();
-        process_lines(reader, search_reg);
-    } else {
-        let f = File::open(input).unwrap();
-        let reader = BufReader::new(f);
-        process_lines(reader, search_reg);
-    }
-}
-
-fn process_lines<T: BufRead + Sized>(reader: T, search_reg: Regex) {
-    for line_ in reader.lines() {
-        let line = line_.unwrap();
-        let contain_substring = search_reg.find(&line);
-
-        match contain_substring {
-            Some(_) => {
-                println!();
-                println!();
-                println!("{}", line)},
-            None => (),
-        }
-    }
+  reader_args()
+    
 }
