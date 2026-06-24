a26eed7fa7bb5f0501e00ae12b6ed106d80c465c
adewaleijalana <adewaleijalana@gmail.com>
2026-06-24 23:08:16 +0100

    add impl From for Q7 struct

--- rust-in-action/grep-lite/src/q.rs
+++ rust-in-action/grep-lite/src/q.rs
@@ -18,3 +18,15 @@ impl From<Q7> for f64 {
         (value.0 as f64) * 2_f64.powf(-7.0)
     }
 }
+
+impl From<f32> for Q7 {
+    fn from(value: f32) -> Self {
+        Q7::from(value as f64)
+    }
+}
+
+impl From<Q7> for f32 {
+    fn from(value: Q7) -> Self {
+        f64::from(value) as f32
+    }
+}
