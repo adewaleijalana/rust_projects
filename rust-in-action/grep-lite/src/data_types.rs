8f7e0d9a81bab5b6c0517cf2610fcd09685419d1
adewaleijalana <adewaleijalana@gmail.com>
2026-06-24 22:06:47 +0100

    overflow example

--- rust-in-action/grep-lite/src/data_types.rs
+++ rust-in-action/grep-lite/src/data_types.rs
@@ -8,9 +8,9 @@ pub fn int_vs_int() {
 
 pub fn transmute_test() {
     let a: f32 = 42.42;
-    // let frankentype: u32 = unsafe { std::mem::transmute(a) }; 
+    // let frankentype: u32 = unsafe { std::mem::transmute(a) };
 
-    let frankentype: u32 = f32::to_bits(a); 
+    let frankentype: u32 = f32::to_bits(a);
 
     //f32::to_bits(a)
 
@@ -18,7 +18,20 @@ pub fn transmute_test() {
     println!("{:032b}", frankentype);
 
     // let b: f32 = unsafe { std::mem::transmute(frankentype) };
-     let b: f32 = unsafe { std::mem::transmute(frankentype) };
+    let b: f32 = unsafe { std::mem::transmute(frankentype) };
     println!("{}", b);
     assert_eq!(a, b);
+}
+
+pub fn int_overflow_example() {
+    let mut i = 0_u16;
+    print!("{}...", i);
+
+    loop {
+        i += 1000;
+        print!("{}...", i);
+        if i % 10000 == 0 {
+            print!("\n")
+        }
+    }
 }
--- rust-in-action/grep-lite/src/main.rs
+++ rust-in-action/grep-lite/src/main.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code, unused_variables, unused_imports)]
 
-use grep_lite::data_types::{int_vs_int, transmute_test};
+use grep_lite::data_types::{int_overflow_example, int_vs_int, transmute_test};
 use grep_lite::file::{File, close, file_test, open};
 use grep_lite::read::Read;
 use grep_lite::reader_args;
@@ -11,5 +11,6 @@ fn main() {
     // file_test();
     // sat_test()
     // int_vs_int();
-    transmute_test();
+    // transmute_test();
+    int_overflow_example();
 }
