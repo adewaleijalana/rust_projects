407bc98354255c9fd646b69268d92d5e11d16abe
adewaleijalana <adewaleijalana@gmail.com>
2026-06-26 08:32:36 +0100

    update to CPU struct

--- rust-in-action/chip_cpu/src/cpu.rs
+++ rust-in-action/chip_cpu/src/cpu.rs
@@ -1,18 +1,24 @@
 pub struct CPU {
-    current_operation: u16,
-    registers: [u8; 2],
+    pub current_operation: u16,
+    position_in_memory: usize,
+    memory: [u8; 0x1000],
 }
 
 impl CPU {
     pub fn new() -> Self {
         Self {
             current_operation: 0,
-            registers: [0, 2],
+            position_in_memory: 0,
+            memory: [0; 0x1000],
         }
     }
 
     pub fn read_opcode(&self) -> u16 {
-        self.current_operation
+        let p = self.position_in_memory;
+        let op_byte1 = self.memory[p] as u16;
+        let op_byte2 = self.memory[p + 1] as u16;
+
+        op_byte1 << 8 | op_byte2
     }
 
     pub fn run(&mut self) {
@@ -32,6 +38,10 @@ impl CPU {
     }
 
     fn add_xy(&mut self, x: u8, y: u8) {
-        self.registers[x as usize] += self.registers[y as usize];
+        // self.registers[x as usize] += self.registers[y as usize];
     }
+
+    // fn add_xy(&mut self, x: u8, y: u8) {
+    //     self.registers[x as usize] += self.registers[y as usize];
+    // }
 }
--- rust-in-action/chip_cpu/src/main.rs
+++ rust-in-action/chip_cpu/src/main.rs
@@ -1,3 +1,15 @@
+use chip_cpu::cpu::CPU;
+
 fn main() {
-    println!("Hello, world!");
+    let mut cpu = CPU::new();
+
+    cpu.current_operation = 0x8014;
+    cpu.registers[0] = 55;
+    cpu.registers[1] = 10;
+
+    cpu.run();
+
+    assert_eq!(cpu.registers[0], 65);
+
+    println!("55 + 10 = {}", cpu.registers[0]);
 }
