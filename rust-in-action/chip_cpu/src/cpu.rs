a00bdacc1350a962585936e0fc4ba1d623a446d2
adewaleijalana <adewaleijalana@gmail.com>
2026-06-27 08:31:45 +0100

    update run method

--- rust-in-action/chip_cpu/src/cpu.rs
+++ rust-in-action/chip_cpu/src/cpu.rs
@@ -1,13 +1,13 @@
 pub struct CPU {
-    resgister: [u8; 16],
+    pub registers: [u8; 16],
     position_in_memory: usize,
-    memory: [u8; 0x1000],
+    pub memory: [u8; 0x1000],
 }
 
 impl CPU {
     pub fn new() -> Self {
         Self {
-            resgister: [0; 16],
+            registers: [0; 16],
             position_in_memory: 0,
             memory: [0; 0x1000],
         }
@@ -22,34 +22,37 @@ impl CPU {
     }
 
     pub fn run(&mut self) {
-        //loop {
-        let opcode = self.read_opcode();
-        let c = ((opcode & 0xF000) >> 12) as u8;
-        let x = ((opcode & 0x0F00) >> 8) as u8;
-        let y = ((opcode & 0x00F0) >> 4) as u8;
-        let d = ((opcode & 0x000F) >> 0) as u8;
+        loop {
+            let opcode = self.read_opcode();
+            self.position_in_memory += 2;
+            println!("opcode: {:0x}", opcode);
+            let c = ((opcode & 0xF000) >> 12) as u8;
+            let x = ((opcode & 0x0F00) >> 8) as u8;
+            let y = ((opcode & 0x00F0) >> 4) as u8;
+            let d = ((opcode & 0x000F) >> 0) as u8;
 
-        match (c, x, y, d) {
-          (0, 0, 0, 0) => { return; },
-            (0x8, _, _, 0x4) => self.add_xy(x, y),
-            _ => todo!("opcode {:04x}", opcode),
+            match (c, x, y, d) {
+                (0, 0, 0, 0) => {
+                    return;
+                }
+                (0x8, _, _, 0x4) => self.add_xy(x, y),
+                _ => todo!("opcode {:04x}", opcode),
+            }
         }
-
-        //}
     }
 
     fn add_xy(&mut self, x: u8, y: u8) {
-       let arg1 = self.resgister[x as usize];
-       let arg2 = self.resgister[y as usize];
+        let arg1 = self.registers[x as usize];
+        let arg2 = self.registers[y as usize];
 
-       let (val, overflow) = arg1.overflowing_add(arg2);
-       self.resgister[x as usize] = val;
+        let (val, overflow) = arg1.overflowing_add(arg2);
+        self.registers[x as usize] = val;
 
-       if overflow {
-           self.resgister[0xf] = 1;
-       }else {
-           self.resgister[0xf] = 0;
-       }
+        if overflow {
+            self.registers[0xf] = 1;
+        } else {
+            self.registers[0xf] = 0;
+        }
     }
 
     // fn add_xy(&mut self, x: u8, y: u8) {
--- rust-in-action/chip_cpu/src/main.rs
+++ rust-in-action/chip_cpu/src/main.rs
@@ -1,24 +1,38 @@
 use chip_cpu::cpu::CPU;
 
 fn main() {
-    // let mut cpu = CPU::new();
+    let mut cpu = CPU::new();
 
-    // cpu.current_operation = 0x8014;
-    // cpu.registers[0] = 55;
-    // cpu.registers[1] = 10;
+    cpu.registers[0] = 5;
+    cpu.registers[1] = 10;
+    cpu.registers[2] = 10;
+    cpu.registers[3] = 10;
 
-    // cpu.run();
+    cpu.memory[0] = 0x80;
+    cpu.memory[1] = 0x14;
+    cpu.memory[2] = 0x80;
+    cpu.memory[3] = 0x24;
+    cpu.memory[4] = 0x80;
+    cpu.memory[5] = 0x34;
 
-    // assert_eq!(cpu.registers[0], 65);
+    let mem = &mut cpu.memory;
+    mem[0] = 0x80;
+    mem[1] = 0x14;
+    mem[2] = 0x80;
+    mem[3] = 0x24;
+    mem[4] = 0x80;
+    mem[5] = 0x34;
 
-    // println!("55 + 10 = {}", cpu.registers[0]);
+    cpu.run();
 
-    test_is_bit_on();
+    assert_eq!(cpu.registers[0], 35);
+
+    println!("5 + 10 + 10 + 10 = {}", cpu.registers[0]);
+
+    // test_is_bit_on();
 }
 
-
 fn test_is_bit_on() {
-
     let arg1 = 23_i64;
     let result = arg1.overflowing_add(23);
 
@@ -32,9 +46,9 @@ fn test_is_bit_on() {
 
     println!("{} | {:0b}", one_at_bit, one_at_bit);
 
-    if one_at_bit != 0{
+    if one_at_bit != 0 {
         println!("not equal to 0");
-    }else {
+    } else {
         println!("equal to 0");
     }
 }
