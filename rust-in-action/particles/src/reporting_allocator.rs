5a13c148407ecc767f5c5c9045283d1508d961f0
adewaleijalana <adewaleijalana@gmail.com>
2026-06-30 21:56:20 +0100

    add ReportingAllocator

--- rust-in-action/particles/src/reporting_allocator.rs
+++ rust-in-action/particles/src/reporting_allocator.rs
@@ -1,1 +1,26 @@
+use std::{alloc::{GlobalAlloc, System}, time::Instant};
+
+
+#[global_allocator]
+static ALLOCATOR: ReportingAllocator = ReportingAllocator;
+
 pub struct ReportingAllocator;
+
+unsafe impl GlobalAlloc for ReportingAllocator {
+    unsafe fn alloc(&self, layout: std::alloc::Layout) -> *mut u8 {
+        let start = Instant::now();
+        let ptr = unsafe { System.alloc(layout) };
+        let end = Instant::now();
+
+        let time_taken = end - start;
+        let bytes_requested = layout.size();
+
+        eprintln!("{}\t{}", bytes_requested, time_taken.as_nanos());
+
+        ptr
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: std::alloc::Layout) {
+        unsafe { System.dealloc(ptr, layout) };
+    }
+}
