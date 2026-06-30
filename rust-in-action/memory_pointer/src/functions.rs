4693f7971d25e60656cacc2f9d5d0f2cb3987a66
adewaleijalana <adewaleijalana@gmail.com>
2026-06-30 21:30:49 +0100

    heap via box

--- rust-in-action/memory_pointer/src/functions.rs
+++ rust-in-action/memory_pointer/src/functions.rs
@@ -79,3 +79,17 @@ pub fn print_addr_5() {
         println!("{:p} -> {:p}", ptr, new_addr);
     }
 }
+
+pub fn heap_via_box() {
+    let a = Box::new(1);
+    let b = Box::new(1);
+    let c = Box::new(1);
+
+    let result1 = *a + *b + *c;
+
+    drop(a);
+    let d = Box::new(1);
+    let result2 = *b + *c + *d;
+
+    println!("{} {}", result1, result2);
+}
--- rust-in-action/memory_pointer/src/main.rs
+++ rust-in-action/memory_pointer/src/main.rs
@@ -2,5 +2,6 @@ use memory_pointer::functions::*;
 fn main() {
     // print_addr();
     // print_addr_2();
-    print_addr_5();
+    // print_addr_5();
+    heap_via_box();
 }
