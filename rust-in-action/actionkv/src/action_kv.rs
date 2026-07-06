f8a61866bade1e58329c75661147de39a612c333
adewaleijalana <adewaleijalana@gmail.com>
2026-07-06 05:16:50 +0100

    load method on ActionKV

--- rust-in-action/actionkv/src/action_kv.rs
+++ rust-in-action/actionkv/src/action_kv.rs
@@ -1,11 +1,11 @@
 use std::{
     collections::HashMap,
     fs::{File, OpenOptions},
-    io::{self},
+    io::{self, BufReader, Seek, SeekFrom},
     path::Path,
 };
 
-use crate::key_value_pair::ByteString;
+use crate::key_value_pair::{ByteString, KeyValuePair};
 
 #[derive(Debug)]
 pub struct ActionKV {
@@ -24,5 +24,31 @@ impl ActionKV {
 
         let index = HashMap::new();
         Ok(ActionKV { file, index })
+    }
+
+    pub fn load(&mut self) -> io::Result<()> {
+        let mut file = BufReader::new(&mut self.file);
+
+        loop {
+            let position = file.seek(SeekFrom::Current(0))?;
+            let maybe_kv = ActionKV::process_record(&mut file);
+
+            let kv = match maybe_kv {
+                Ok(kv) => kv,
+                Err(err) => match err.kind() {
+                    io::ErrorKind::UnexpectedEof => {
+                        break;
+                    }
+                    _ => return Err(err),
+                },
+            };
+            self.index.insert(kv.key, position);
+        }
+
+        Ok(())
+    }
+
+    pub fn process_record<R: io::Read + Seek>(reader: &mut R) -> io::Result<KeyValuePair> {
+        Ok(KeyValuePair { key: (), value: () })
     }
 }
